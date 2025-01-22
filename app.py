615b960bd3169ea66cacb64f14b2768dd080a0dc
manik singh <109017266+mksgh@users.noreply.github.com>
2025-01-22 01:25:37 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from transformers import pipeline
+import gradio as gr
+
+# use bart in pytorch
+model = pipeline("summarization")
+
+
+def predict(prompt):
+    summary = model(prompt)[0]["summary_text"]
+    return summary
+
+
+with gr.Blocks() as demo:
+    textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
+    gr.Interface(fn=predict, inputs=textbox, output="text")
+
+demo.launch()
